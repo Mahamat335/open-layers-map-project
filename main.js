057c890190dcc7a2da12506b2850e21e274ac77d
Mahamat335 <gcanbolat46@gmail.com>
2022-08-17 11:53:57 +0300

    Çalışan ilk sürüm

--- main.js
+++ main.js
@@ -1,64 +1,183 @@
-//
-// Define rotate to north control.
-//
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var RotateNorthControl = /** @class */ (function (_super) {
-    __extends(RotateNorthControl, _super);
-    /**
-     * @param {Object} [opt_options] Control options.
-     */
-    function RotateNorthControl(opt_options) {
-        var _this = this;
-        var options = opt_options || {};
-        var button = document.createElement('button');
-        button.innerHTML = 'N';
-        var element = document.createElement('div');
-        element.className = 'rotate-north ol-unselectable ol-control';
-        element.appendChild(button);
-        _this = _super.call(this, {
-            element: element,
-            target: options.target
-        }) || this;
-        button.addEventListener('click', _this.handleRotateNorth.bind(_this), false);
-        return _this;
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {Draw, Modify, Snap} from 'ol/interaction';
+import {GeometryCollection, Point, Polygon} from 'ol/geom';
+import {circular} from 'ol/geom/Polygon';
+import {getDistance} from 'ol/sphere';
+import {transform} from 'ol/proj';
+
+const geodesicStyle = new Style({
+  geometry: function (feature) {
+    return feature.get('modifyGeometry') || feature.getGeometry();
+  },
+  fill: new Fill({
+    color: 'rgba(0, 255, 255, 0.2)',
+  }),
+  stroke: new Stroke({
+    color: '#ff3333',
+    width: 2,
+  }),
+  image: new CircleStyle({
+    radius: 7,
+    fill: new Fill({
+      color: 'rgba(255, 255, 0, 0)',
+    }),
+  }),
+});
+
+const style = new Style({
+  fill: new Fill({
+    color: 'rgba(255, 255, 255, 0.2)',
+  }),
+  stroke: new Stroke({
+    color: '#33cc33',
+    width: 2,
+  }),
+  image: new CircleStyle({
+    radius: 7,
+    fill: new Fill({
+      color: '#aacc33',
+    }),
+  }),
+});
+const source = new VectorSource({
+  format: new GeoJSON(),
+  url: './data/countries.json',
+  style: function (feature) {
+    const geometry = feature.getGeometry();
+    return geometry.getType() === 'GeometryCollection' ? geodesicStyle : style;
+  },
+});
+
+const map = new Map({
+  target: 'map-container',
+  layers: [
+    new VectorLayer({
+      source: source,
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
+const defaultStyle = new Modify({source: source})
+  .getOverlay()
+  .getStyleFunction();
+
+const modify = new Modify({
+  source: source,
+  style: function (feature) {
+    feature.get('features').forEach(function (modifyFeature) {
+      const modifyGeometry = modifyFeature.get('modifyGeometry');
+      if (modifyGeometry) {
+        const modifyPoint = feature.getGeometry().getCoordinates();
+        const geometries = modifyFeature.getGeometry().getGeometries();
+        const polygon = geometries[0].getCoordinates()[0];
+        const center = geometries[1].getCoordinates();
+        const projection = map.getView().getProjection();
+        let first, last, radius;
+        if (modifyPoint[0] === center[0] && modifyPoint[1] === center[1]) {
+          // center is being modified
+          // get unchanged radius from diameter between polygon vertices
+          first = transform(polygon[0], projection, 'EPSG:4326');
+          last = transform(
+            polygon[(polygon.length - 1) / 2],
+            projection,
+            'EPSG:4326'
+          );
+          radius = getDistance(first, last) / 2;
+        } else {
+          // radius is being modified
+          first = transform(center, projection, 'EPSG:4326');
+          last = transform(modifyPoint, projection, 'EPSG:4326');
+          radius = getDistance(first, last);
+        }
+        // update the polygon using new center or radius
+        const circle = circular(
+          transform(center, projection, 'EPSG:4326'),
+          radius,
+          128
+        );
+        circle.transform('EPSG:4326', projection);
+        geometries[0].setCoordinates(circle.getCoordinates());
+        // save changes to be applied at the end of the interaction
+        modifyGeometry.setGeometries(geometries);
+      }
+    });
+    return defaultStyle(feature);
+  },
+});
+
+modify.on('modifystart', function (event) {
+  event.features.forEach(function (feature) {
+    const geometry = feature.getGeometry();
+    if (geometry.getType() === 'GeometryCollection') {
+      feature.set('modifyGeometry', geometry.clone(), true);
     }
-    RotateNorthControl.prototype.handleRotateNorth = function () {
-        this.getMap().getView().setRotation(0);
+  });
+});
+
+modify.on('modifyend', function (event) {
+  event.features.forEach(function (feature) {
+    const modifyGeometry = feature.get('modifyGeometry');
+    if (modifyGeometry) {
+      feature.setGeometry(modifyGeometry);
+      feature.unset('modifyGeometry', true);
+    }
+  });
+});
+
+map.addInteraction(modify);
+
+let draw, snap; // global so we can remove them later
+const typeSelect = document.getElementById('type');
+
+function addInteractions() {
+  let value = typeSelect.value;
+  let geometryFunction;
+  if (value === 'Geodesic') {
+    value = 'Circle';
+    geometryFunction = function (coordinates, geometry, projection) {
+      if (!geometry) {
+        geometry = new GeometryCollection([
+          new Polygon([]),
+          new Point(coordinates[0]),
+        ]);
+      }
+      const geometries = geometry.getGeometries();
+      const center = transform(coordinates[0], projection, 'EPSG:4326');
+      const last = transform(coordinates[1], projection, 'EPSG:4326');
+      const radius = getDistance(center, last);
+      const circle = circular(center, radius, 128);
+      circle.transform('EPSG:4326', projection);
+      geometries[0].setCoordinates(circle.getCoordinates());
+      geometry.setGeometries(geometries);
+      return geometry;
     };
-    return RotateNorthControl;
-}(ol.control.Control));
-//
-// Create map, giving it a rotate to north control.
-//
-var maxExtent = [25, 35.5, 45, 42.5];
-var centerpos = [31, 43.5];
-var newpos = ol.proj.transform(centerpos, 'EPSG:4326', 'EPSG:900913');
-var baseLayerOSM = new ol.layer.Tile({
-    source: new ol.source.OSM(),
-    isBaseLayer: true
-});
-var map = new ol.Map({
-    layers: [baseLayerOSM],
-    target: 'map',
-    view: new ol.View({
-        extent: ol.proj.transformExtent(maxExtent, 'EPSG:4326', 'EPSG:900913'),
-        projection: 'EPSG:900913',
-        center: newpos,
-        minZoom: 6,
-        zoom: 0
-    })
-});
+  }
+  draw = new Draw({
+    source: source,
+    type: value,
+    geometryFunction: geometryFunction,
+  });
+  map.addInteraction(draw);
+  snap = new Snap({source: source});
+  map.addInteraction(snap);
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  map.removeInteraction(snap);
+  addInteractions();
+};
+
+addInteractions();
